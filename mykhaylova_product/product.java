8d98d9fb8a3ff7e2294d815e141dd9a2e50402ae
mykhaylova <mariya_lvovna@yahoo.com>
2016-05-03 23:07:02 +0200

    Product class

---
+++ mykhaylova_product/product.java
@@ -0,0 +1,21 @@
+package mykhaylova_product;
+
+public class Product
+{
+	private String name;
+	
+	public Product()
+	{
+		setName("Coca Cola");
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}	
+}
